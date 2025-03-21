2cfe86cd3a4acb8ee6726bc1996c1719b4d5cefb
Emmanuel Yakubu <tethcode@gmail.com>
2025-03-22 00:01:30 +0100

    Update urls.py

--- shortener/urls.py
+++ shortener/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import shorten_url, redirect_url
+from django.http import JsonResponse
 
+def home(request):
+    return JsonResponse({"message": "URL Shortener API is working!"})
 
 urlpatterns = [
+    path("", home),
     path("api/shorten/", shorten_url, name="api-shorten"),
     path("<str:short_code>/", redirect_url, name="redirect")
 ]
